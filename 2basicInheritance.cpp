720d16c5f8a006ecce871fa8edc925e7e0ec153d
shubham chauhan <154400060+shubh56666@users.noreply.github.com>
2024-09-02 09:46:34 +0530

    Add files via upload

--- 2basicInheritance.cpp
+++ 2basicInheritance.cpp
@@ -1,22 +1,29 @@
 #include<iostream>
 using namespace std;
-class Human
-{
-
-    public:
+class Human{
+    protected:
     string name;
-    int age,weight;
-};
+    int age;
 
+};
 class Student : public Human
 {
-    private:
-int roll_no,fees;
+    int roll_no,fees;
+    public:
+    Student(string name,int age,int roll_no,int fees)
+    {
+        this->name = name;
+        this->age = age;
+        this->roll_no =  roll_no;
+        this->fees = fees;
+    }
+    void display(){
+        cout<<name<<" "<<age<<" "<<roll_no<<" "<<fees<<endl;
+    }
 
 };
-int main(){
-  
-Student A;
-A.name;
-
+int main()
+{
+    Student a1("Rohit",34,234,700);
+    a1.display();
 }

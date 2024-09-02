31f3b60631dd9b97d99e7ee02e2e4b3dec9b7c93
shubham chauhan <154400060+shubh56666@users.noreply.github.com>
2024-09-02 10:26:16 +0530

    Add files via upload

---
+++ 8singleinheritance.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class Human{
+    protected:
+    string name;
+    int age;
+
+};
+class Student : public Human
+{
+    int roll_no,fees;
+    
+    public:
+    Student(string name,int age,int roll_no,int fees)
+    {
+        this->name = name;
+        this->age = age;
+        this->roll_no =  roll_no;
+        this->fees = fees;
+    }
+    void display(){
+        cout<<name<<" "<<age<<" "<<roll_no<<" "<<fees<<endl;
+    }
+
+};
+int main()
+{
+    Student a1("Rohit",12,23,700);
+    a1.display();
+}
